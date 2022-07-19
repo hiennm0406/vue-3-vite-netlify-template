551df434f7cbcc84cbfc609fbb04feff44cfed07
hiennm0406 <hiennm0406@gmail.com>
2022-07-19 16:26:29 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+Vue.prototype.$axios = axios
+Vue.prototype.$api_url = "https://jsonplaceholder.typicode.com/"
 
 createApp(App).mount('#app')
