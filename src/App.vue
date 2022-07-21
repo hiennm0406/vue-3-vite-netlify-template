da8ad93a281dd9224c01309c8c07e02a637889a5
hiennm0406 <hiennm0406@gmail.com>
2022-07-21 11:15:36 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,143 @@
-<script setup>
+
+<template>
+  <sidebar-menu @update:collapsed="onToggleCollapse" :menu="menu" />
+  <router-view />
+</template>
+
+<script >
 
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Sidebar from './components/Sidebar.vue'
+export default {
+  data: function() {
+    return {
+      users: [],
+      //https://yaminncco.github.io/vue-sidebar-menu/#/basic-usage
+      menu: [
+          {
+            header: 'RAGNAROK',
+            hiddenOnCollapse: true
+          },
+          {
+            to: '/',
+            title: 'Main Page',
+            icon: {
+              element: 'span',
+              class: 'icon40 icon_main',
+            }
+          },
+          {
+            href: { name: 'History' },
+            title: 'Lịch sử thế giới',
+            icon: 'fa fa-chart-area',
+            child: [
+              {
+                href: { name: 'firstRagnarok' },
+                title: 'The first ragnarok'
+              }
+            ]
+          },
+          {
+            href: { name: 'Setting' },
+            title: 'Tổng quan - Setting',
+            icon: 'fa fa-chart-area',
+            child: [
+              {
+                href: '/',
+                title: 'Atribute'
+              },
+               {
+                href: '/',
+                title: 'Skill'
+              },
+              {
+                href: '/',
+                title: 'Skill link'
+              },
+              {
+                href: '/',
+                title: 'Tactic'
+              },
+              {
+                href: '/',
+                title: 'Artifact'
+              },
+              {
+                href: '/',
+                title: 'Rune'
+              },
+              {
+                href: '/',
+                title: 'Trait'
+              },
+              {
+                href: '/',
+                title: 'Awake'
+              },
+              
+            ]
+          },
+          {
+            href: { name: 'Character' },
+            title: 'Nhân vật',
+            icon: 'fa fa-chart-area',
+            child: [
+               {
+                href: '/',
+                title: 'Dkyrie'
+              },
+              {
+                href: '/',
+                title: 'Midgard'
+              },
+               {
+                href: '/',
+                title: 'Asgard'
+              },
+              {
+                href: '/',
+                title: 'Alfheim'
+              },
+              {
+                href: '/',
+                title: 'Vanaheim'
+              },
+              {
+                href: '/',
+                title: 'Muspelheim'
+              },
+              {
+                href: '/',
+                title: 'Svartalfheim'
+              },
+              {
+                href: '/',
+                title: 'Jotunheim'
+              },
+              {
+                href: '/',
+                title: 'Nilfheim'
+              },
+              {
+                href: '/',
+                title: 'Helheim'
+              },
+            ]
+          }
+        ]
+    };
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
+    },
+    onToggleCollapse(collapsed) {
+      alert(1);
+    },
+  }
+}
 </script>
-
-<template>
-  <Sidebar></Sidebar>
-  <router-view />
-</template>
 
 <style>
 @import "./css/global.css";
