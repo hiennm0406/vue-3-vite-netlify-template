a7c7c30c25303d5f34bc8f9c73b0194128b6c776
hiennm0406 <hiennm0406@gmail.com>
2022-07-21 11:06:44 +0700

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -34,6 +34,10 @@ const routes = [
         component: Character,
         name: 'Character',
     },
+    {
+        path: '/:catchAll(.*)',
+        component: Main
+    },
 ]
 
 const router = createRouter({
