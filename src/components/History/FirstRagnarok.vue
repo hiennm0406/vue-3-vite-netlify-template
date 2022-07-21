9dce6200cbe41af4b9c726caa48c38bd2d7e5505
hiennm0406 <hiennm0406@gmail.com>
2022-07-21 10:53:28 +0700

    up

---
+++ src/components/History/FirstRagnarok.vue
@@ -0,0 +1,45 @@
+<template>
+   <h1>Ragnarok lần đầu tiên</h1>
+
+   Từ xa xưa, xưa còn hơn cả những gì con người có thể lật ngược lại được. Những thực thể cổ đại ấy đã đặt chân tới Trái Đất.
+Họ là những thực thể thông thái, mạnh mẽ và vĩ đại. Những cá thể nhỏ nhất của họ cũng cao hơn 20m, với làn da xanh sần và lớp vảy cứng dày, đủ để cho họ chịu đựng những bức sóng vũ trụ khi họ di chuyển qua các miền thế giới.
+Vào thời điểm đấy, các cánh cổng vẫn còn mở, những cánh cổng - hay họ - người Jotun - gọi chúng là Bifrost, ý chỉ cầu vồng. Một cách gọi thơ mộng cho những vết rách không gian vặn vẹo và phun ra bức xạ vũ trụ bảy màu. Những vết rách đó là những lỗ giun, bằng một phép màu của vũ trụ, nó nối thông 9 hành tinh, ở 9 hệ mặt trời khác nhau. Những vết rách đó không ổn định, lúc đóng lúc mở, đủ kích thước, nhưng rồi đã có những vết rách đủ to để Ymir, một người Jotun vĩ đại chui qua và bước chân lên Trái Đất.
+
+<image scr="./../../assets/giant.jpg"></image>
+
+Thế rồi, như một cơn lũ, nền văn minh Jotun trải dài khắp 9 giới, họ phát triển như một phép màu, nghiên cứu các thảm thực vật và sinh vật.
+Họ dùng gen của họ, tạo ra khủng long.
+Họ cũng phát hiện ra Alfheim, một ngôi sao chết với phóng xạ không sao sống được. Họ nghiên cứu một loại thực vật có thể hấp thu phóng xạ và trồng vào đó.
+Họ phát hiện ra thế giới đại dương Nilfheim, và họ xây dựng ở đó hòn đảo Alantic khổng lồ. Họ tạo ra một chủng tộc khổng lồ gọi là Culthul để lặn xuống những vùng đất sâu nhất mà họ không thể tới được.
+Họ cũng phát hiện ra Asgard, nơi được bao trùm bởi một loại vật chất đặc biệt mà họ nghĩ nó chỉ tồn tại ở xác của một hố đen. Ymir đã xây dựng một khu mỏ ở đây để khai thác và nghiên cứu nó, họ đặt tên nó là Valhalla.
+Nền văn minh của họ trải dài khắp 9 thế giới.
+Cho đến ngày đó, người Jotun gọi là ngày [Khải hoàn], hay [Ngày chia cắt].
+Một ngôi sao chổi vĩ đại lao thẳng vào Trái Đất. Vụ nổ xóa sạch nền văn minh Jotun trên hành tinh xanh, kéo theo đó là vô số sự sống.
+Nhưng không chỉ thế, sự va chạm phá vỡ sự ổn định của không gian, những vết rách đóng lại.
+Sự chia cắt đó của người Jotun nhanh chóng khiến cả một nền văn minh sụp đổ. Những dòng thức ăn từ Midgard phân chia đến các thành phố khác của họ bị chặn lại, khiến cho người Jotun rải rác khắp các thế giới khác phải vật lộn với tự nhiên để sinh tồn.
+Ở Midgard, nơi đáng ra có nền văn minh tân tiến nhất, thì hứng chịu cơn thịnh nộ của vũ trụ. Ngôi sao chổi xóa xổ hai phần ba động thực vật trên Trái Đất, nhấn chìm Trái Đất vào trong bóng tối lạnh lẽo bởi tro bụi núi lửa phủ khắp bầu trời. Người Jotun trên Midgard lúc này chỉ còn biết co cụm lại trên một thành phố nằm phía Nam lục địa, nơi còn sót lại những cánh rừng rậm rạp.
+Họ đang chết dần trong Ragnarok đầu tiên, trong ba mùa đông dài đằng đẵng.
+Cho đến khi, một tạo vật nhỏ của họ mở mắt.
+Họ gọi chúng là Manna. Thứ sinh vật nhỏ bé, yếu ớt, trần trụi, không có vảy cứng rắn như họ. Nhưng các sinh vật đó thông minh, ham học hỏi. Chúng tò mò với mọi thứ, với lửa, với ánh sáng, và với chính những thực thể đã tạo ra chúng.
+Sự thông minh của chúng làm người Jotun dè chừng. Phải rồi, ai mà muốn những miếng thịt biết đi bé nhỏ của mình thông minh hơn chứ.
+Mọi thứ diễn ra đúng như những gì người Jotun muốn, suốt hàng triệu năm, con người trở thành thực phẩm, trở thành công cụ, trở thành nô lệ. Nền văn minh Jotun ngỡ như đã trở lại, phồn hoa hơn. Nhưng thực chất, nó đang bị ăn mòn, bị mục ruỗng từ sâu thẳm bên trong, nền văn minh Jotun từ một nền văn minh hiền hòa, yêu khám phá, thích sáng tạo thế giới và chiêm ngưỡng nó phát triển, trở thành một nền văn minh nuôi cấy, giết thịt, một giống loài hủy diệt.
+Với những tia nắng ló ra từ bầu trời, báo hiệu mùa đông của tro bụi sắp chấm dứt. Cũng là một kỷ nguyên mới bắt đầu.
+Ba Manna, với cái tên Odin, Vili, and Vé, đã đứng lên, dẫn dắt những Manna khác, cầm vũ khí, nhỏ bé và thô sơ, tấn công vào thành phố Jotun. Những Jotun bất ngờ không kịp làm gì cho đến lúc sụp đổ. Ymir bị chết bởi một thanh kiếm đâm xuyên qua mắt. Thực thể Jotun vĩ đại nhất đã chết như thế, báo hiệu một cái kết cho nền văn minh Jotun kéo dài hàng trăm triệu năm…
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+    };
+  } 
+}
+ 
+</script>
