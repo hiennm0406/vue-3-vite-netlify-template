03c2fc9e8b9f54ba791cf4bde4a369d782d6c641
hiennm0406 <hiennm0406@gmail.com>
2022-07-26 10:07:05 +0700

    Update Character001.vue

--- src/components/Character/Dkyrie/Character001.vue
+++ src/components/Character/Dkyrie/Character001.vue
@@ -8,7 +8,7 @@
       <td class="text_left">
         <h2>Stat:</h2>
         <br/>
-        <Slider v-model="value" :max="15" />
+        <Slider v-model="value" :max="15" :min="1" />
         <h3><b>Might: </b>{{might}}</h3><br/>
         <h3><b>Energy: </b>{{energy}}</h3><br/>
         <h3><b>Endurance: </b>{{endurance}}</h3><br/>
@@ -53,7 +53,7 @@ export default {
   data: function() {
     return {
       users: [],
-      value:0,
+      value:1,
       might:0,
       energy:0,
       endurance:0,
@@ -64,12 +64,11 @@ export default {
   methods: {
     changeValue(){
       console.log(this.value);
-      this.might = this.increase(this.dataChar.Stat.Might,this.dataChar.Stat.MightEvo,this.value);
-      this.energy = this.increase(this.dataChar.Stat.Energy,this.dataChar.Stat.EnergyEvo,this.value);
-      this.endurance = this.increase(this.dataChar.Stat.Endurance,this.dataChar.Stat.EnduranceEvo,this.value);
+      this.might = this.increase(this.dataChar.Stat.Might,this.dataChar.Stat.MightEvo,this.value-1);
+      this.energy = this.increase(this.dataChar.Stat.Energy,this.dataChar.Stat.EnergyEvo,this.value-1);
+      this.endurance = this.increase(this.dataChar.Stat.Endurance,this.dataChar.Stat.EnduranceEvo,this.value-1);
     },
     increase(input,type,level){
-      console.log(json.StatEvolution.semi[14]);
       for (let index = 0; index < level; index++) {
        if(type == "early"){
         input = input * json.StatEvolution.early[index];
