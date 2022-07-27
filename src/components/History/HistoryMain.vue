fa79d407ba58af78ac2ec3eb865b25119e0ba31d
hiennm0406 <hiennm0406@gmail.com>
2022-07-27 09:29:12 +0700

    u

--- src/components/History/HistoryMain.vue
+++ src/components/History/HistoryMain.vue
@@ -2,8 +2,19 @@
    <h1>Kỷ xa xưa</h1>
    <ul>
       <li><router-link :to="{ name: 'firstRagnarok'}">Ragnarokr lần đầu tiên</router-link></li>
-      <li> <router-link :to="{ name: 'TheUnion'}">Ngày tái hợp</router-link></li>
+      <li><router-link :to="{ name: 'TheUnion'}">Ngày tái hợp</router-link></li>
    </ul>
+
+   <div id="myTimeline">
+  <ul class="timeline-events">
+    <li data-timeline-node="{ start:'2019-02-26 10:00',end:'2019-02-26 13:00',content:'<p>Event Body...</p>' }">Event Label</li>
+    <li data-timeline-node="{ start:'2019-03-01 23:10',end:'2019-03-02 1:30' }">
+      <span class="event-label">Event Label</span>
+      <span class="event-content"><p>Event Body...</p></span>
+    </li>
+  </ul>
+</div>
+<div class="timeline-event-view"></div>
 </template>
 
 <style scoped>
@@ -11,7 +22,11 @@ a {
   color: #42b983;
 }
 </style>
-
+<script>
+$(function () {
+    $("#myTimeline").Timeline()
+})
+</script>
 
 <script>
 export default {
