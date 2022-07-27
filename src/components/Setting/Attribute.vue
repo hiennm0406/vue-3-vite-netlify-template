e52c973a1a1da594a3710a86a4e82d6d11208f7f
hiennm0406 <hiennm0406@gmail.com>
2022-07-27 22:20:34 +0700

    Update Attribute.vue

--- src/components/Setting/Attribute.vue
+++ src/components/Setting/Attribute.vue
@@ -1,6 +1,16 @@
 <template>
    <h2>Attribute</h2>
 
+<p>Mỗi nhân vật có 3 Attribute chính, 3 attribute này kiểm soát gần như toàn bộ các chỉ số chính của trò chơi.</p>
+<p>Ngoài ra nhân vật còn có các Attribute phụ khác.</p>
+
+<h3>Main Attribute</h3>
+<ul>
+  <li>Might</li>
+  <li>Enegry</li>
+  <li>Endurance</li>
+</ul>
+
    
 </template>
 
