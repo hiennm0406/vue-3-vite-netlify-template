b5ab6fa05404fb5a578ba305931ea48c836042f5
hiennm0406 <hiennm0406@gmail.com>
2022-07-28 13:16:41 +0700

    u

--- src/components/Setting/Tactic.vue
+++ src/components/Setting/Tactic.vue
@@ -12,7 +12,37 @@
    <span class="tactic"></span>
    <span class="tactic"></span>
 </div>
+<p>Lúc bắt đầu trận đấu, sẽ có một đội được chỉ định đi trước. Thường là người chơi.</p>
+<p>Lúc này, người chơi sẽ lựa chọn các đồng minh và gán từng nhân vật vào các ô Tactic ngẫu nhiên. Tối đa mỗi đội sẽ gán được vào 4 ô Tactic.</p>
 
+<table class="table_content">
+  <tr>
+    <td><img class="tactic_img" src="../../assets/character/demo1.png"/></td>
+    <td><img class="tactic_img" src="../../assets/character/demo2.png"/></td>
+    <td></td>
+    <td><img class="tactic_img" src="../../assets/character/demo5.png"/></td>
+    <td><img class="tactic_img" src="../../assets/character/demo6.png"/></td>
+  </tr>
+  <tr>
+    <td><img class="tactic_img" src="../../assets/character/demo3.png"/></td>
+    <td><img class="tactic_img" src="../../assets/character/demo4.png"/></td>
+    <td></td>
+    <td><img class="tactic_img" src="../../assets/character/demo7.png"/></td>
+    <td><img class="tactic_img" src="../../assets/character/demo8.png"/></td>
+  </tr>
+  <tr>
+    <td colspan="5">
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+    </td>
+  </tr>
+</table>
 
 </template>
 
