b78d4dc0398ef8fa9a74b945055d52d5c8986bbe
hiennm0406 <hiennm0406@gmail.com>
2022-07-28 13:28:43 +0700

    Update Tactic.vue

--- src/components/Setting/Tactic.vue
+++ src/components/Setting/Tactic.vue
@@ -20,6 +20,37 @@
   </tr>
   <tr>
     <td colspan="5">
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+      <span class="tactic"></span>
+    </td>
+  </tr>
+</table>
+<p>Lúc bắt đầu trận đấu, sẽ có một đội được chỉ định đi trước. Thường là người chơi.</p>
+<p>Lúc này, người chơi sẽ lựa chọn các đồng minh và gán từng nhân vật vào các ô Tactic ngẫu nhiên. Tối đa mỗi đội sẽ gán được vào 4 ô Tactic.</p>
+
+<table>
+  <tr>
+    <td><img class="tactic_img ally" src="../../assets/character/demo1.png"/></td>
+    <td><img class="tactic_img ally" src="../../assets/character/demo2.png"/></td>
+    <td><div style="width:100px;"></div></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo5.png"/></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo6.png"/></td>
+  </tr>
+  <tr>
+    <td><img class="tactic_img ally" src="../../assets/character/demo3.png"/></td>
+    <td><img class="tactic_img ally" src="../../assets/character/demo4.png"/></td>
+    <td></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo7.png"/></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo8.png"/></td>
+  </tr>
+  <tr>
+    <td colspan="5">
       <img class="tactic_img ally" src="../../assets/character/demo1.png"/>
       <img class="tactic_img ally" src="../../assets/character/demo1.png"/>
       <span class="tactic"></span>
@@ -31,8 +62,6 @@
     </td>
   </tr>
 </table>
-<p>Lúc bắt đầu trận đấu, sẽ có một đội được chỉ định đi trước. Thường là người chơi.</p>
-<p>Lúc này, người chơi sẽ lựa chọn các đồng minh và gán từng nhân vật vào các ô Tactic ngẫu nhiên. Tối đa mỗi đội sẽ gán được vào 4 ô Tactic.</p>
 
 <p>Sau khi đội đi trước hoàn thành việc xếp Tactic. Đội thứ 2 sẽ bắt đầu gán nhân vật của họ vào các ô trống còn lại.</p>
 <table>
