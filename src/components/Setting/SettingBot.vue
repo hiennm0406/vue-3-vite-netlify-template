ef37eb2c994c773d87c916e2ca822d0b5587596b
hiennm0406 <hiennm0406@gmail.com>
2022-07-28 16:14:50 +0700

    Update SettingBot.vue

--- src/components/Setting/SettingBot.vue
+++ src/components/Setting/SettingBot.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="data_container">
     <ul>
-          <li><router-link :to="{ name: 'level'}">Level</router-link></li>
-          <li><router-link :to="{ name: 'attribute'}">Attribute</router-link></li>
-           <li><router-link :to="{ name: 'skill'}">Skill</router-link></li>
-          <li><router-link :to="{ name: 'trait'}">Trait</router-link></li>
-           <li><router-link :to="{ name: 'ascen'}">Ascensions</router-link></li>
-        </ul>
+        <li>&#2022;<router-link :to="{ name: 'level'}">Level</router-link></li>
+        <li>&#2022;<router-link :to="{ name: 'attribute'}">Attribute</router-link></li>
+        <li>&#2022;<router-link :to="{ name: 'skill'}">Skill</router-link></li>
+        <li>&#2022;<router-link :to="{ name: 'trait'}">Trait</router-link></li>
+        <li>&#2022;<router-link :to="{ name: 'ascen'}">Ascensions</router-link></li>
+      </ul>
   </div>
 </template>
 
 <style scoped>
 .data_container{
-    padding: 0 50px;
+  display:inline;     
+    padding: 0 20px;
     border: 1px solid #00000017;
     border-radius: 20px;
     box-shadow: 0px 0px 10px 0px;
+}
+ul li{
+  display: inline;
 }
 </style>
 
