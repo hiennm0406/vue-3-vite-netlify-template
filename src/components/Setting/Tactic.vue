1ec5e0f4f32318c944f4de0f47eef924662456b1
hiennm0406 <hiennm0406@gmail.com>
2022-07-28 13:27:26 +0700

    u

--- src/components/Setting/Tactic.vue
+++ src/components/Setting/Tactic.vue
@@ -2,44 +2,64 @@
    <h2>Tactic</h2>
 
 <p>Tactic - chiến thuật. Là một thanh gồm 8 ô trống.</p>
-<div>
-   <span class="tactic"></span>
-   <span class="tactic"></span>
-   <span class="tactic"></span>
-   <span class="tactic"></span>
-   <span class="tactic"></span>
-   <span class="tactic"></span>
-   <span class="tactic"></span>
-   <span class="tactic"></span>
-</div>
-<p>Lúc bắt đầu trận đấu, sẽ có một đội được chỉ định đi trước. Thường là người chơi.</p>
-<p>Lúc này, người chơi sẽ lựa chọn các đồng minh và gán từng nhân vật vào các ô Tactic ngẫu nhiên. Tối đa mỗi đội sẽ gán được vào 4 ô Tactic.</p>
 
 <table>
   <tr>
-    <td><img class="tactic_img" src="../../assets/character/demo1.png"/></td>
-    <td><img class="tactic_img" src="../../assets/character/demo2.png"/></td>
+    <td><img class="tactic_img ally" src="../../assets/character/demo1.png"/></td>
+    <td><img class="tactic_img ally" src="../../assets/character/demo2.png"/></td>
     <td><div style="width:100px;"></div></td>
-    <td><img class="tactic_img" src="../../assets/character/demo5.png"/></td>
-    <td><img class="tactic_img" src="../../assets/character/demo6.png"/></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo5.png"/></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo6.png"/></td>
   </tr>
   <tr>
-    <td><img class="tactic_img" src="../../assets/character/demo3.png"/></td>
-    <td><img class="tactic_img" src="../../assets/character/demo4.png"/></td>
+    <td><img class="tactic_img ally" src="../../assets/character/demo3.png"/></td>
+    <td><img class="tactic_img ally" src="../../assets/character/demo4.png"/></td>
     <td></td>
-    <td><img class="tactic_img" src="../../assets/character/demo7.png"/></td>
-    <td><img class="tactic_img" src="../../assets/character/demo8.png"/></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo7.png"/></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo8.png"/></td>
   </tr>
   <tr>
     <td colspan="5">
+      <img class="tactic_img ally" src="../../assets/character/demo1.png"/>
+      <img class="tactic_img ally" src="../../assets/character/demo1.png"/>
       <span class="tactic"></span>
       <span class="tactic"></span>
+      <img class="tactic_img ally" src="../../assets/character/demo3.png"/>
       <span class="tactic"></span>
+      <img class="tactic_img ally" src="../../assets/character/demo2.png"/>
       <span class="tactic"></span>
-      <span class="tactic"></span>
-      <span class="tactic"></span>
-      <span class="tactic"></span>
-      <span class="tactic"></span>
+    </td>
+  </tr>
+</table>
+<p>Lúc bắt đầu trận đấu, sẽ có một đội được chỉ định đi trước. Thường là người chơi.</p>
+<p>Lúc này, người chơi sẽ lựa chọn các đồng minh và gán từng nhân vật vào các ô Tactic ngẫu nhiên. Tối đa mỗi đội sẽ gán được vào 4 ô Tactic.</p>
+
+<p>Sau khi đội đi trước hoàn thành việc xếp Tactic. Đội thứ 2 sẽ bắt đầu gán nhân vật của họ vào các ô trống còn lại.</p>
+<table>
+  <tr>
+    <td><img class="tactic_img ally" src="../../assets/character/demo1.png"/></td>
+    <td><img class="tactic_img ally" src="../../assets/character/demo2.png"/></td>
+    <td><div style="width:100px;"></div></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo5.png"/></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo6.png"/></td>
+  </tr>
+  <tr>
+    <td><img class="tactic_img ally" src="../../assets/character/demo3.png"/></td>
+    <td><img class="tactic_img ally" src="../../assets/character/demo4.png"/></td>
+    <td></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo7.png"/></td>
+    <td><img class="tactic_img enemy" src="../../assets/character/demo8.png"/></td>
+  </tr>
+  <tr>
+    <td colspan="5">
+      <img class="tactic_img ally" src="../../assets/character/demo1.png"/>
+      <img class="tactic_img ally" src="../../assets/character/demo1.png"/>
+      <img class="tactic_img enemy" src="../../assets/character/demo5.png"/>
+      <img class="tactic_img enemy" src="../../assets/character/demo8.png"/>
+      <img class="tactic_img ally" src="../../assets/character/demo3.png"/>
+      <img class="tactic_img enemy" src="../../assets/character/demo6.png"/>
+      <img class="tactic_img ally" src="../../assets/character/demo2.png"/>
+      <img class="tactic_img enemy" src="../../assets/character/demo5.png"/>
     </td>
   </tr>
 </table>
