c8b7ea0be0a332ce34254d59234eeac78e12d363
hiennm0406 <hiennm0406@gmail.com>
2022-07-28 14:53:49 +0700

    Update Skill.vue

--- src/components/Setting/Skill.vue
+++ src/components/Setting/Skill.vue
@@ -63,7 +63,7 @@
   <button :class="{active:active=='Type'}" @click="active='Type'">Type</button>
   <button :class="{active:active=='Cost'}" @click="active='Cost'">Cost</button>
   <button :class="{active:active=='Description'}" @click="active='Description'">Description</button>
-  <button :class="{active:active=='Tag'}" @click="active='Description'">Tag</button>
+  <button :class="{active:active=='Tag'}" @click="active='Tag'">Tag</button>
 </div>
 <div v-if="active=='Type'" class="data_container" >
   <h3>Tất cả skill của các nhân vật được chia làm 2 loại chính:</h3>
